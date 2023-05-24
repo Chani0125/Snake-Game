bef9e9f35f1de02dda38caf590a058f947e653a7
clzlol <nyc0527@naver.com>
2023-05-24 11:30:19 +0900

    start project

---
+++ snakeGame/main.cpp
@@ -0,0 +1,16 @@
+#include <ncurses/ncurses.h>
+#include <iostream>
+
+int main(){
+    initscr();
+    start_color();
+    init_pair(1,COLOR_RED,COLOR_WHITE);
+    attron(COLOR_PAIR(1));
+    printw("hello");
+    attroff(COLOR_PAIR(1));
+    refresh();
+    getch();
+    endwin();
+    
+    return 0;
+}
