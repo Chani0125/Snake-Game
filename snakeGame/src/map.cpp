632c312c913a03f1ebda57962a61f0f095e50068
Chani0125 <ap25ap25@naver.com>
2023-06-18 15:12:18 +0900

    Complete Loading Map

--- snakeGame/src/map.cpp
+++ snakeGame/src/map.cpp
@@ -4,11 +4,10 @@
 #include <vector>
 #include <string>
 
-#define FILENAME(n) "map" + to_string(n) + ".txt"
+#define FILENAME(n) "./map/map" + to_string(n) + ".txt"
 
 using namespace std;
 
-// vector<vector<vector<int>>> map = vector<vector<vector<int>>>(5, vector<vector<int>>(5, vector<int>(5, 5)));
 vector<vector<vector<int>>> map;
 
 int loadMapFile()
@@ -16,15 +15,7 @@ int loadMapFile()
     int fileNum = 0;
     ifstream file;
 
-    file.open("map0.txt");
-    cout << "map0.txt" << " is opening" << "\n";
-    if (file.fail())
-        cout << "map0.txt" << " is not opened" << "\n\n";
-
-    // file.open(FILENAME(fileNum));
-    // cout << FILENAME(fileNum) << " is opening" << "\n";
-    // if (file.fail())
-    //     cout << FILENAME(fileNum) << " is not opened" << "\n\n";
+    file.open(FILENAME(fileNum));
     
     while(file.is_open())
     {
@@ -34,8 +25,6 @@ int loadMapFile()
             continue;
         }
 
-        cout << FILENAME(fileNum) << " is successfully opened" << "\n\n";
-
         int x = 0, y = 0;
         string map_str_line;
         vector<vector<int>> tmp_map;
@@ -43,12 +32,10 @@ int loadMapFile()
         getline(file, map_str_line);
         y = map_str_line.length();
 
-        cout << "str: " << map_str_line << "\n";
-
         vector<int> tmp_map_line(y, 2);
 
         for (int i = 0; i < y; i++)
-            tmp_map_line[i] = map_str_line[i];
+            tmp_map_line[i] = map_str_line[i] - '0';
         tmp_map.push_back(tmp_map_line); x++;
 
 
@@ -57,7 +44,7 @@ int loadMapFile()
         {
             getline(file, map_str_line);
             for (int i = 0; i < y; i++)
-                tmp_map_line[i] = map_str_line[i];
+                tmp_map_line[i] = map_str_line[i] - '0';
             tmp_map.push_back(tmp_map_line); x++;
         }
 
