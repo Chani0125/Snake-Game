4d9067fdafd27652148d4c03366c1c700c6e06e0
clzlol <nyc0527@naver.com>
2023-06-18 23:48:18 +0900

    setup draw

--- snakeGame/src/draw.cpp
+++ snakeGame/src/draw.cpp
@@ -17,16 +17,32 @@ void Draw()
     attron(COLOR_PAIR(1));
     wbkgd(playing_map, COLOR_PAIR(1));
 
+    WINDOW *score_board = subwin(stdscr, play_map.size()/2-3, play_map[0].size()/2+5, 5, 7+play_map[0].size());
+    init_pair(2, COLOR_BLACK, COLOR_GREEN);
+    attron(COLOR_PAIR(2));
+    wbkgd(score_board, COLOR_PAIR(2));
 
-    // printw("[Play Map %d]\n", played_stage_history.back());
-    // mvwprintw(stdscr, 0, 0, "[Play Map %d]\n", played_stage_history.back());
+    WINDOW *mission_board = subwin(stdscr, play_map.size()/2-3, play_map[0].size()/2+5, 5+play_map.size()/2, 7+play_map[0].size());
+    init_pair(2, COLOR_BLACK, COLOR_GREEN);
+    attron(COLOR_PAIR(2));
+    wbkgd(mission_board, COLOR_PAIR(2));
 
-    for(int i = 0; i < play_map.size()-1; i++){
-        for(int j = 0; j < play_map[i].size()-1; j++){
-            wprintw(playing_map, "%d", play_map[i][j]);
+
+
+    for(auto x : play_map){
+        for(int y : x){
+            if(y >= 0)
+            wprintw(playing_map, "%d", y);
         }
-        // wprintw(playing_map, "\n");
+        wprintw(playing_map, "\n");
     }
+
+    wprintw(score_board, "current score");
+
+    wprintw(mission_board, "body length\n");
+    wprintw(mission_board, "growth\n");
+    wprintw(mission_board, "poison\n");
+    wprintw(mission_board, "gate\n");
 
     refresh();
     delwin(playing_map);
