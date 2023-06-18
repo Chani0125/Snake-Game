48d753bb47012fb90929c55642e90065ef0ebb10
clzlol <nyc0527@naver.com>
2023-06-18 21:29:11 +0900

    add snake's movement

--- snakeGame/src/snake.h
+++ snakeGame/src/snake.h
@@ -1,16 +1,24 @@
-class snake
+#ifndef _SNAKE_
+#define _SNAKE_
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Snake
 {
 private:
-    /* data */
+    bool alive;
+    int direction; // 1,2,3,4 - north, east, south, west
+    vector<pair<int,int>> body; //row,col
 public:
-    snake(/* args */);
-    ~snake();
+    Snake(/* args */);
+    pair<int, int> getHead();
+    bool move();
+    bool move(pair<int, int>);
+    bool move(int direction);
+    ~Snake();
 };
 
-snake::snake(/* args */)
-{
-}
-
-snake::~snake()
-{
-}
+#endif
